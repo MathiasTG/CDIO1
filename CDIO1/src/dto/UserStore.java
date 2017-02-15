ea76380f5855624199c57b4abfd93d02e9e8b6e0
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-02-15 14:11:33 +0100

    checkCpr by Morten og Jonas

--- CDIO1/src/dto/UserStore.java
+++ CDIO1/src/dto/UserStore.java
@@ -1,5 +1,8 @@
 package dto;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class UserStore {
 	
@@ -9,6 +12,36 @@ public class UserStore {
 		
 	}
 	
+	public boolean checkCpr(String cpr){
+		Date date = null;
+		//First try and catch for "-" error 
+		try{
+		String[] parts = cpr.split("-");
+		String dateNumber = parts[0]; //
+		String number = parts[1]; //
+		
+		if(dateNumber.length()==6 && number.length()==4){
+			try {
+				SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy");
+				date = sdf.parse(dateNumber);
+				if (!dateNumber.equals(sdf.format(date))) {
+					return false;
+				}
+				else{
+					return true;
+				}
+			} catch (ParseException ex) {
+				return false;
+			}	
+			
+		}
+		else 
+			return false;
+		
+		}catch (ArrayIndexOutOfBoundsException e){
+			return false;
+		}
+	}
 	
 
 }
