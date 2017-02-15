bd81de990628c49c9800efb75a7c4b08b8029c81
s153374 <s153374@DTU-CZC6268C83.win.dtu.dk>
2017-02-15 14:23:07 +0100

    date

--- CDIO1/src/dto/UserStore.java
+++ CDIO1/src/dto/UserStore.java
@@ -7,7 +7,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
 import dal.IUserDAO;
 import exceptions.DALException;
 
