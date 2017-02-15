3be49cc262c04760ff566bd03419017ccd2690cc
s153374 <s153374@DTU-CZC6268C83.win.dtu.dk>
2017-02-15 13:36:31 +0100

    set and get

--- CDIO1/src/dto/UserStore.java
+++ CDIO1/src/dto/UserStore.java
@@ -1,11 +1,45 @@
 package dto;
 import java.util.ArrayList;
+import java.util.List;
 
-public class UserStore {
-	
-	private ArrayList<String> users;
+import dal.IUserDAO;
+
+public class UserStore implements IUserDAO {
+		
+	private List<UserDTO> users;
 	
 	public UserStore() {
+		
+
+	}
+
+	@Override
+	public UserDTO getUser(int userId) throws DALException {
+	
+		return null;
+	}
+
+	@Override
+	public List<UserDTO> getUserList() throws DALException {
+	
+		return null;
+	}
+
+	@Override
+	public void createUser(UserDTO user) throws DALException {
+	
+		
+	}
+
+	@Override
+	public void updateUser(UserDTO user) throws DALException {
+		
+		
+	}
+
+	@Override
+	public void deleteUser(int userId) throws DALException {
+	
 		
 	}
 	
