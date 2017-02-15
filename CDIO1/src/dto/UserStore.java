9b2d532b80b45f550781f31969a6821709cfe3e3
Sofie <Sofie@Sofies_Pc.eduroam.wireless.dtu.dk>
2017-02-15 14:16:45 +0100

    File

--- CDIO1/src/dto/UserStore.java
+++ CDIO1/src/dto/UserStore.java
@@ -1,15 +1,25 @@
 package dto;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import dal.IUserDAO;
+import exceptions.DALException;
 
 public class UserStore implements IUserDAO {
 		
 	private List<UserDTO> users;
+	private String theString = "The value of that string";
+    private int    someInteger = 0;
 	
 	public UserStore() {
-		
+	   
+	   
 
 	}
 
@@ -43,6 +53,26 @@ public class UserStore implements IUserDAO {
 		
 	}
 	
-	
+public static void main( String [] args ) throws IOException  { 
 
+	String aString = "The value of that string";
+    int    someInteger = 999;
+   // SS instance = new SS();
+    ObjectOutputStream oos = new ObjectOutputStream( 
+                           new FileOutputStream(new File("UserInfo.ser")));
+
+    // do the magic  
+    oos.writeObject( aString + " " + someInteger );
+    // close the writing.
+    oos.close();
+    ObjectInputStream ois = new ObjectInputStream(
+			new FileInputStream("UserInfo.ser"));
+    try {
+		System.out.println(""+ ois.readObject());
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    ois.close();
+    	}
 }
