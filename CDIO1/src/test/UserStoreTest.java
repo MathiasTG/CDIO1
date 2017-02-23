4e59e9a8ab42aba42c3274737ea47259b6e44b49
Jonas Larsen <jonaslarsen@10.16.138.92>
2017-02-23 21:37:44 +0100

    Sikke noget lort

--- CDIO1/src/test/UserStoreTest.java
+++ CDIO1/src/test/UserStoreTest.java
@@ -1,100 +1,60 @@
 package test;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import dto.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import dto.UserDTO;
-import dto.UserStore;
-
 public class UserStoreTest {
 	private List<UserDTO> users;
 	String pathName = "testData.ser";
-//	@Before
-//	public void setup() {
-//		UserStore data = new UserStore(pathName);
-//		loadInfo();
-//
-//	}
+	private UserStore data = new UserStore(pathName);
+	List<String> roles;
+	UserDTO user1, user2, user3;
+	boolean testStatus = false;
 
-//	@After
-//	public void teardown() {
-//
-//	}
+	@Before
+	public void setup() {
+
+		roles = new ArrayList<String>();
+		roles.add("Admin");
+		user1 = new UserDTO(11, "DetteErNumber1", "EN", "111111-1111", "dsafadddf", roles);
+		user2 = new UserDTO(12, "DetteErNumber2", "EN", "221111-1111", "la,fsasds", roles);
+
+		user3 = new UserDTO(13, "DetteErNumber3", "EN", "121111-1111", "asfdad", roles);
+		
+	}
+
+	// @After
+	// public void teardown() {
+	//
+	// }
 
 	@Test
 	public void testDatabase() {
-		List<String> roles = new ArrayList<String>();
-
-		roles.add("Admin");
-
-		UserDTO user1 = new UserDTO(15, "djiasof adsf", "dsf2", "111111-1111", "dsafadf", roles);
-
-		users.add(user1);
-		saveInfo();
-		List<UserDTO> tempUsers = users;
-		users = null; 
-		loadInfo();
-	
-		Assert.assertEquals(tempUsers, users);
-
-	}
-
-	public void saveInfo() {
-		try {
-			OutputStream file = new FileOutputStream(pathName);
-			OutputStream buffer = new BufferedOutputStream(file);
-			ObjectOutput output = new ObjectOutputStream(buffer);
-			// ObjectOutputStream oos = new ObjectOutputStream(new
-			// FileOutputStream(new File("UserInfo.ser")));
-			output.writeObject(users);
-			// close the writing.
-			output.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	public void loadInfo() {
+		data.loadInfo();
 
 		try {
-			InputStream file = new FileInputStream(pathName);
-			InputStream buffer = new BufferedInputStream(file);
-			ObjectInput input = new ObjectInputStream(buffer);
-			// ois = new ObjectInputStream(new FileInputStream("UserInfo.ser"));
-			users = (ArrayList<UserDTO>) input.readObject();
-			input.close();
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (EOFException e) {
-			users = new ArrayList();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			data.createUser(user1);
+
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("taber");
+
 		}
+
+		try {
+			if (data.getUser(11).getCpr().equals("11")) {
+				testStatus = true;
+			}
+		} catch (Exception e) {
+			System.out.println("Mega taber");
+		}
+		Assert.assertTrue(testStatus);
+
 	}
 
 }
